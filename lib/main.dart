6be379a4c1c9dc438da71402fe0d66d647671c96
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2021-07-10 21:56:20 +0900

    todoList.mapを作成しました

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_app/main_model.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,25 +11,33 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Todoアプリ',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Todoアプリ'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'You have pushed the button this many times:',
-              ),
-            ],
+      home: ChangeNotifierProvider<MainModel>(
+        create: (_) => MainModel()..getTodoList(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: StreamBuilder<Object>(
+              stream: null,
+              builder: (context, snapshot) {
+                return Text('Todoアプリ');
+              }
+            ),
           ),
+          body: Consumer<MainModel>(builder: (context, model, child) {
+            final todoList = model.todoList;
+            return ListView(
+              children: todoList.map(
+                  (todo) => ListTile(
+                    title: Text(todo.title),
+                ),
+              ).toList(),
+            );
+          }),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          ), // This trailing comma makes auto-formatting nicer for build methods.
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          tooltip: 'Increment',
-          child: Icon(Icons.add),
-        ), // This trailing comma makes auto-formatting nicer for build methods.
       ),
     );
   }
